9537dbcfd8bae249d97e46602a5cb5f1779abdcb
Browncow2020 <connectwithbob@icloud.com>
2018-09-23 10:57:37 -0500

    First commit

---
+++ src/app/03-setup-and-teardown/vote.component.spec.ts
@@ -0,0 +1,24 @@
+import { VoteComponent } from './vote.component'; 
+
+describe('VoteComponent', () => {
+  let component: VoteComponent;
+
+  beforeEach(() => {
+    component = new VoteComponent();
+  });
+
+  afterEach(() => {
+    // do clean up here if needed
+  });
+
+  it('should increment total votes when upvoted', () => {
+    component.upVote();
+    expect(component.totalVotes).toBe(1);
+  });
+
+  it('should decrement total votes when downvoted', () => {
+    component.downVote();
+    expect(component.totalVotes).toBe(-1);
+  });
+
+});
---
+++ src/app/04-forms/todo-form.component.spec.ts
@@ -0,0 +1,23 @@
+import { FormBuilder } from '@angular/forms';
+import { TodoFormComponent } from './todo-form.component';
+
+
+describe('TodoFormComponent', () => {
+  let component: TodoFormComponent;
+
+  beforeEach(() => {
+    component = new TodoFormComponent(new FormBuilder());
+  });
+
+  it('should create a new form with two form controls', () => {
+    expect(component.form.contains('name')).toBeTruthy();
+  });
+
+  it('should make the name control required', () => {
+    const control = component.form.get('name');
+
+    control.setValue('');
+
+    expect(control.valid).toBeFalsy();
+  });
+});
